1af1af8e0592524b32f0a44e96fa1bffc608d904
Pedro Ribeiro <pedroribeiro@fastmail.com>
2025-09-22 08:16:53 -0300

    Removing unused imports

--- src/test/java/com/qa/pribeiro/exercicioAula02/steps/DashboardSteps.java
+++ src/test/java/com/qa/pribeiro/exercicioAula02/steps/DashboardSteps.java
@@ -11,8 +11,6 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.chrome.ChromeDriver;
 import org.openqa.selenium.chrome.ChromeOptions;
-import org.openqa.selenium.support.ui.ExpectedConditions;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 import com.qa.pribeiro.exercicioAula02.helpers.ScreenshotHelper;
 
